77c14b07fe17c55963e291c4ea6a2343b6571ff1
João Pedro Alves de Souza <joao987656@gmail.com>
2026-05-08 20:21:36 +0100

    first commit

---
+++ src/test/java/com/recipes/api/Recipes_API/RecipesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipes.api.Recipes_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
